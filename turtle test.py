32df92b27af2cf95695833df296891c26fb5f2a0
Gabriel Noah Bernier <gabenoah8@gmail.com>
2023-04-15 17:57:03 -0400

    Infinite Turtle Square

--- turtle test.py
+++ turtle test.py
@@ -1,3 +1,20 @@
 from turtle import *
 
 shape('turtle')
+
+n = 5
+
+while n > 0:
+
+    forward(50)
+    left(90)
+    forward(50)
+    left(90)
+    forward(50)
+    left(90)
+    forward(50)
+    left(90)
+    forward(10)
+    right(90)
+    forward(10)
+    left(90)
