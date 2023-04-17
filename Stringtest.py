e030eaaef45aaabce36b4a8c4739ffcd31f42498
Gabriel Noah Bernier <125150185+GabN-Kamper@users.noreply.github.com>
2023-04-17 09:10:16 -0400

    Added time import

--- Stringtest.py
+++ Stringtest.py
@@ -1,4 +1,8 @@
+import time
+
 food = input('What is your favourite food?')
 name = input('What is your name?')
 
-print(name, 'hates throwing away', food)
+print(name, 'hates', food)
+time.sleep(3)
+print('jk')
