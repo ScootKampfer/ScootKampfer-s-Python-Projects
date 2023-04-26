cc5e6302918f1d142f87995d379fe9464eb69829
Gabriel Noah Bernier <gabenoah8@gmail.com>
2023-04-25 20:24:32 -0400

    among us

--- turtle test.py
+++ turtle test.py
@@ -1,20 +1,75 @@
 from turtle import *
+import time
+n = 0
+m = 0
+p = 0
 
 shape('turtle')
 
-n = 5
+#we are making an among us drawing, which is a bit sus
 
-while n > 0:
+forward(30)
+left(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(65)
 
-    forward(50)
-    left(90)
-    forward(50)
-    left(90)
-    forward(50)
-    left(90)
-    forward(50)
-    left(90)
-    forward(10)
-    right(90)
-    forward(10)
-    left(90)
+left(90)
+forward(10)
+
+while m < 181:
+
+    forward(0.17452777777)
+    right(1)
+    m = m + 1
+
+forward(20)
+
+while p < 181:
+
+    forward(0.17452777777)
+    right(1)
+    p = p + 1
+
+forward(20)
+m = 0
+
+while m < 181:
+
+    forward(0.17452777777)
+    right(1)
+    m = m + 1
+
+forward(10)
+
+left(90)
+
+
+forward(20)
+
+while n < 181:
+
+    forward(0.78539816339)
+    left(1)
+    n = n + 1
+    
+forward(100)
+back(50)
+right(90)
+forward(20)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(90)
+
+time.sleep(5)
+
+quit()
